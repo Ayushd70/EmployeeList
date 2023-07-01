8bfe20ac3e23904071b6fff97c6ed5f0743b39a6
Ayush Dubey <ayushdubey70@gmail.com>
2023-07-01 15:31:51 +0530

    employee_list: models: button model
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ lib/models/button_model.dart
@@ -0,0 +1,28 @@
+import 'package:employee_list/configs/resources.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+enum ButtonType {
+  textButton,
+  iconButton,
+}
+
+class ButtonDataModel {
+  Size size = Size(AppResources.width, 40);
+  Color buttonColor = AppColors.primaryColor;
+  String? text;
+  String? icon;
+  Function function;
+  ButtonType buttonType;
+  Color? textColor;
+
+  ButtonDataModel({
+    required this.size,
+    required this.buttonColor,
+    required this.text,
+    required this.icon,
+    required this.function,
+    required this.buttonType,
+    required this.textColor,
+  });
+}
