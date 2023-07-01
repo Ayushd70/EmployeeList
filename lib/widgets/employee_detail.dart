519fbba3a428b97e2dadecce0bf8ea7a0280ca3d
Ayush Dubey <ayushdubey70@gmail.com>
2023-07-01 15:48:24 +0530

    employee_list: widgets: employee details widgets
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ lib/widgets/employee_detail.dart
@@ -0,0 +1,35 @@
+import 'package:employee_list/configs/resources.dart';
+import 'package:flutter/material.dart';
+
+import '../models/empoloyee_details_model.dart';
+import '../utils/app_helper.dart';
+
+class EmployeeDataWidget extends StatelessWidget {
+  const EmployeeDataWidget({super.key, required this.employeeDetails});
+  final EmployeeDetailsDataModel employeeDetails;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          AppHelper.toCamelCase(employeeDetails.employeeName),
+          style: AppTextStyles.mediumRoboto16
+              .copyWith(color: AppColors.secondaryColor),
+        ),
+        const SizedBox(
+          height: 6,
+        ),
+        Text(AppHelper.toCamelCase(employeeDetails.designation),
+            style: AppTextStyles.regularRoboto14
+                .copyWith(color: AppColors.lightgray)),
+        const SizedBox(
+          height: 6,
+        ),
+        Text(AppHelper.toCamelCase(employeeDetails.employeementPeriod),
+            style: AppTextStyles.regularRoboto14
+                .copyWith(color: AppColors.lightgray)),
+      ],
+    );
+  }
+}
