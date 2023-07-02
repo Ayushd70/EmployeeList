f9d126c81961b68da12cc7eb06bd63d0698692bd
Ayush Dubey <ayushdubey70@gmail.com>
2023-07-02 21:35:16 +0530

    employee_list: db: setup local database
    * db model and table
    * use sqfentity
    
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ lib/local_db_models/employees_local_db_model.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:sqfentity/sqfentity.dart';
+import 'package:sqfentity_gen/sqfentity_gen.dart';
+part 'employees_local_db_model.g.dart';
+
+const SqfEntityTable employeesTable = SqfEntityTable(
+    tableName: 'employees',
+    primaryKeyName: 'id',
+    primaryKeyType: PrimaryKeyType.integer_auto_incremental,
+    useSoftDeleting: true,
+    fields: [
+      SqfEntityField('name', DbType.text),
+      SqfEntityField('designation', DbType.text),
+      SqfEntityField('period', DbType.text)
+    ]);
+
+const seqIdentity = SqfEntitySequence(
+  sequenceName: 'identity',
+);
+
+@SqfEntityBuilder(employeesModel)
+const employeesModel = SqfEntityModel(
+  modelName: 'employeesModel', // optional
+  databaseName: 'employeeManagement-db.db',
+  password: null,
+
+  // put defined sequences into the sequences list.
+  sequences: [seqIdentity],
+
+  // put defined tables into the tables list.
+  databaseTables: [employeesTable],
+);
